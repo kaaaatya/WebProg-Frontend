252729c0ff611f0cc828fb6c9b5374f095451d23
PerepelitsunaEE <43075958+PerepelitsunaEE@users.noreply.github.com>
2019-10-11 10:01:22 +0400

    lab2lab3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,58 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { InfoComponent } from './info/info.component';
+import { BookComponent } from './book/book.component';
+import { AuthorComponent } from './author/author.component';
+import { CollectionComponent } from './collection/collection.component';
+
+const appRoutes: Routes = [
+  { 
+    path: 'home', 
+    component: LandingComponent,
+    data: { title: 'Главная' } 
+  },
+  { 
+    path: 'info',  
+    component: InfoComponent,
+    data: { title: 'О сайте' }
+  },
+  { 
+    path: 'book',  
+    component: BookComponent,
+    data: { title: 'Книги' }
+  },
+  { 
+    path: 'author',  
+    component: AuthorComponent,
+    data: { title: 'Автор' }
+  },
+  { 
+    path: 'collection',  
+    component: CollectionComponent,
+    data: { title: 'Коллекция' }
+  },
+  
+  //{ path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    InfoComponent,
+    BookComponent,
+    AuthorComponent,
+    CollectionComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     AppRoutingModule
   ],
